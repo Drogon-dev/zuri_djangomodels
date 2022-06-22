5fbd366d215aea59e4f5739333011575e80361c7
Drogon-dev <okenedaniel44@gmail.com>
2022-06-22 14:48:30 +0100

    hot fix

--- I4G0047827ZT/blog/models.py
+++ I4G0047827ZT/blog/models.py
@@ -5,11 +5,10 @@ from django.contrib.auth import get_user_model
 class Post(models.Model):
     Title = models.CharField(max_length=200)
     Text = models.TextField()
-    Author = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
+    Author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     Created_date = models.DateTimeField()
     Published_date = models.DateTimeField()
 
     
     def __str__(self) -> str:
         return self.Title
-
